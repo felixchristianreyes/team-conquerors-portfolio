7aead1792130175de2ed772a4a9ef1c21011124b
Niño Abelardo Brillo <brillo.nino@gmail.com>
2022-07-12 03:47:27 +0800

    added ComponentRed

--- src/components/ComponentRed/ComponentRed.test.js
+++ src/components/ComponentRed/ComponentRed.test.js
@@ -1,15 +1,3 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-
-import { ComponentRed } from './ComponentRed';
-
-describe('<Contact />', () => {
-  test('it should mount', () => {
-    render(<ComponentRed />);
-    
-    const contact = screen.getByTestId('Contact');
-
-    expect(contact).toBeInTheDocument();
-  });
-});
+import PropTypes from 'prop-types';
+import styles from './Contact.module.css';
